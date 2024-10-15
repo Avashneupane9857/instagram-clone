50232a13a9c1711845dd2bf68e6bb0d80ca78330
Avashneupane9857 <juisch20220011252@gmail.com>
2024-10-15 23:58:56 +0530

    feat: getUserPost controller

--- backend/controllers/post_controllers.js
+++ backend/controllers/post_controllers.js
@@ -1,5 +1,5 @@
 import sharp from "sharp";
-import cloudinary from "../utils/cloudinary";
+import cloudinary from "../utils/cloudinary.js";
 import { Post } from "../models/post_model.js";
 import { User } from "../models/user_model.js";
 export const addNewPost = async (req, res) => {
@@ -7,6 +7,8 @@ export const addNewPost = async (req, res) => {
     const caption = req.body.caption;
     const image = req.file;
     const authorId = req.id;
+
+    console.log(authorId);
     console.log(authorId, caption);
     if (!image) {
       return res.status(401).json({
@@ -67,3 +69,42 @@ export const getAllPost = async (req, res) => {
     console.log(e);
   }
 };
+export const getUserPost = async (req, res) => {
+  try {
+    const authorId = req.id;
+    console.log(authorId);
+    const post = await Post.find({ author: authorId })
+      .sort({ createdAt: -1 })
+      .populate({ path: "author", select: "username, profilePic " })
+      .populate({
+        path: "comments",
+        sort: { createdAt: -1 },
+        populate: {
+          path: "author",
+          select: "username,profilePic",
+        },
+      });
+    return res.send(200).json({
+      post,
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const likePost = async (req, res) => {
+  try {
+    const whoLikes = req.id;
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({
+        msg: "No post found",
+        success: false,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+//Likes part controller remaining
--- backend/routes/post_route.js
+++ backend/routes/post_route.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { addNewPost } from "../controllers/post_controllers.js";
+import isAuth from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
-router.post("/addnewpost", addNewPost);
+router.post("/addnewpost", isAuth, addNewPost);
 export default router;
